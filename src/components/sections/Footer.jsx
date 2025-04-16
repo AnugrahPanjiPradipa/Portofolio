5ff1cfa5c1f7ecf1801a72f613bb19c16b2857dc
AnugrahPanjiPradipa <panjipradipa714@gmail.com>
2025-04-16 20:31:58 +0700

    Edit footer

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -6,13 +6,13 @@ export const Footer = () => {
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
         <p className="text-sm">&copy; 2025 Anugrah Panji Pradipa. All rights reserved.</p>
         <div className="flex space-x-4 mt-4 md:mt-0">
-          <a href="#" className="hover:text-gray-400">
+          <a href="https://github.com/AnugrahPanjiPradipa" className="hover:text-gray-400">
             <Github size={24} />
           </a>
-          <a href="#" className="hover:text-gray-400">
+          <a href="https://www.linkedin.com/in/anugrah-panji-pradipa/" className="hover:text-gray-400">
             <Linkedin size={24} />
           </a>
-          <a href="#" className="hover:text-gray-400">
+          <a href="https://www.instagram.com/apidarp_/" className="hover:text-gray-400">
             <Instagram size={24} />
           </a>
         </div>
